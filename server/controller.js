478d5fe459b5378ad7d081cdf8efe02e5d5ba4d0
Arash Zaheri <arashzaheri@gmail.com>
2021-06-15 18:49:16 -0500

    Final without error

--- server/controller.js
+++ server/controller.js
@@ -2,15 +2,11 @@ const houses = require('./db.json') //auto fills with json file when using house
 let globalId = 4;
 
 module.exports = {
-    getAllHouses: (req, res) => res.status(200).send(houses), //can be houses
-    deleteHouse: function (req,res) {
-        const targetIndex = houses.findIndex(function (houseObj) {
-            return houseObj.id === +req.params.houseId
-        });
-
-        houses.splice(targetIndex, 1)
-        res.status(200).send(houses)
-       
+    getHouses: (req, res) => res.status(200).send(houses), //can be houses
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
+        houses.splice(index, 1)//cuts out index given and what 
+        res.status(200).send(houses)    
         //keep terms consistent
     }, //finds index of element given
     createHouse: (req, res) => {
@@ -30,16 +26,12 @@ module.exports = {
         let { type } = req.body
         let index = houses.findIndex(elem => +elem.id === +id)
 
-        if (houses[index].price === 0 && type === 'minus') {
-            res.sendStatus(400)
-        }
-        else if ( type === 'plus' ) {
+        if ( type === 'plus' ) {
             houses[index].price += 10000
             res.status(200).send(houses);
         } else if ( type === 'minus') {
             houses[index].price -= 10000
-        } else {
-            res.statusSend(400)
+            res.status(200).send(houses);
         }
 // account for negative
     }
--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const app = express();
 const cors = require('cors')
 const ctrl = require('./controller.js') //needs to be ./ to call file controller.js
 
-const { getAllHouses, deleteHouse, createHouse, updateHouse } = ctrl
+const { getHouses, deleteHouse, createHouse, updateHouse } = ctrl
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/houses', getAllHouses)
-app.delete('/api/houses/:houseId', deleteHouse)
+app.get('/api/houses', getHouses)
+app.delete('/api/houses/:id', deleteHouse)
 app.post('/api/houses', createHouse)
-app.put('/api/houses/:houseId', updateHouse)
+app.put('/api/houses/:id', updateHouse)
 
 serverPort = 4005;
 app.listen(serverPort, function(){
